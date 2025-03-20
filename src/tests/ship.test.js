58ce6619ff993768957b216a7eb8915ca76f7e07
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-03-20 15:08:14 +0000

    Create Ship class with tests

---
+++ src/tests/ship.test.js
@@ -0,0 +1,34 @@
+import { Ship } from "../modules/ship.js";
+
+let ship;
+
+beforeEach(() => {
+  ship = new Ship(3);
+});
+
+/* testing the instance initialization is actually not necessary as the constructor only creates instance variables but does not call any method */
+
+test("instance initialization", () => {
+  expect(ship).toEqual({ length: 3, hitCount: 0, sunk: false });
+});
+
+test("ship.hit() increases ship.hitCount by 1", () => {
+  ship.hit();
+  expect(ship.hitCount).toBe(1);
+});
+
+describe("if ship.hitCount equals ship.length", () => {
+  test("ship.isSunk() sets ship.sunk to true", () => {
+    ship.hitCount = 3;
+    ship.isSunk();
+    expect(ship.sunk).toBe(true);
+  });
+});
+
+describe("if ship.hitCount does NOT equal ship.length", () => {
+  test("ship.isSunk() does NOT change ship.sunk (remains false)", () => {
+    ship.hitCount = 2;
+    ship.isSunk();
+    expect(ship.sunk).toBe(false);
+  });
+});
