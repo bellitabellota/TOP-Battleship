ab43c9b333065e181a2017fef5375a4d30297cfd
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-03-21 14:12:45 +0000

    Add preliminary method for placing Ships

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import { DOMController } from "./modules/dom-controller.js";
 const game = new Game();
 game.initializePlayers("Player1", "Computer");
 game.setInitialCurrentPlayer(0);
+game.placeShips();
 
 const dom = new DOMController(game);
 dom.displayPlayerNames();
 dom.displayCurrentPlayerBoard();
 dom.displayCurrentOpponentBoard();
+
+console.log(game.currentPlayer.board.current);
+console.log(game.currentOpponent().board.current);
--- src/modules/game.js
+++ src/modules/game.js
@@ -1,4 +1,5 @@
 import { HumanPlayer, ComputerPlayer } from "./player.js";
+import { Ship } from "./ship.js";
 
 export class Game {
   constructor() {
@@ -28,5 +29,32 @@ export class Game {
 
   currentOpponent() {
     return this.currentPlayer = this.players[0] ? this.players[1] : this.players[0];
+  }
+
+  /* test method below once it is finally implemented */
+  placeShips() {
+    const carrier = new Ship(5);
+    const battleship = new Ship(4);
+    const destroyer = new Ship(3);
+    const submarine = new Ship(3);
+    const patrolBoat = new Ship(2);
+
+    const coordinatesCarrier = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]];
+    const coordinatesBattleShip = [[2, 2], [2, 3], [2, 4], [2, 5]];
+    const coordinatesDestroyer = [[5, 3], [5, 4], [5, 5]];
+    const coordinatesSubmarine = [[7, 3], [7, 4], [7, 5]];
+    const coordinatesPatrolBoat = [[9, 4], [9, 5]];
+
+    this.players[0].board.placeShip(coordinatesCarrier, carrier);
+    this.players[0].board.placeShip(coordinatesBattleShip, battleship);
+    this.players[0].board.placeShip(coordinatesDestroyer, destroyer);
+    this.players[0].board.placeShip(coordinatesSubmarine, submarine);
+    this.players[0].board.placeShip(coordinatesPatrolBoat, patrolBoat);
+
+    this.players[1].board.placeShip(coordinatesCarrier, carrier);
+    this.players[1].board.placeShip(coordinatesBattleShip, battleship);
+    this.players[1].board.placeShip(coordinatesDestroyer, destroyer);
+    this.players[1].board.placeShip(coordinatesSubmarine, submarine);
+    this.players[1].board.placeShip(coordinatesPatrolBoat, patrolBoat);
   }
 }
