ef601a24fcf91b3410529c0e5c4ae091de714b6b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-03-22 12:59:03 +0000

    Start implementing making the Computer's move

--- src/modules/dom-controller.js
+++ src/modules/dom-controller.js
@@ -19,12 +19,23 @@ export class DOMController {
       opponentFieldElements.forEach((field) => {
         field.addEventListener("click", () => {
           const coordinate = this.getClickedCoordinate(field);
-
+          
           this.game.currentOpponent().board.receiveAttack(coordinate);
 
           this.displayCurrentOpponentBoard(); /* this call also removes the EventListeners as the updated board gets re-created on the DOM */
 
           this.game.switchCurrentPlayer();
+
+          /* make ComputerMove */
+          let coordinateComputer;
+
+          do {
+            coordinateComputer = this.game.currentPlayer.getCoordinateChoice();
+          } 
+          while(!this.game.isChoiceValid(coordinateComputer));
+
+          this.game.currentOpponent().board.receiveAttack(coordinateComputer);
+          
         });
       });
     }
@@ -32,7 +43,6 @@ export class DOMController {
     getClickedCoordinate(field) {
       const xCoordinate = Number(field.dataset.index.split("")[0]);
       const yCoordinate = Number(field.dataset.index.split("")[1]);
-      console.log([xCoordinate, yCoordinate]);
       return [xCoordinate, yCoordinate];
     }
 
