3b6c9dd023ee0ee515f5ac86559986b16f039251
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-03-23 12:06:32 +0000

    Implement and test getRandomInteger() helper

---
+++ src/modules/random-integer.js
@@ -0,0 +1,4 @@
+export function getRandomInteger() {
+  //returns a random integer between 0 and 9 (both included)
+  return Math.floor(Math.random() * 10);
+}
---
+++ src/tests/random-integer.test.js
@@ -0,0 +1,11 @@
+import { getRandomInteger } from "../modules/random-integer";
+
+test("returns a number between 0 and 9 (both included)", () => { 
+  for (let i = 0; i < 100; i++) {
+    const randomNumber = getRandomInteger();
+    expect(Number.isInteger(randomNumber)).toBe(true);
+    
+    expect(randomNumber).toBeGreaterThanOrEqual(0);
+    expect(randomNumber).toBeLessThanOrEqual(9);
+  }
+})
